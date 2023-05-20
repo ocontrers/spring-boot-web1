d265218a6654a86c4d1a3f7cda68b178df53bd3c
Oscar Contreras <osccontrer.s@gmail.com>
2023-05-20 09:34:30 -0600

    Se agrega el package Controller y la class IndexController

---
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -0,0 +1,19 @@
+package com.bolsadeideas.springboot.web.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+//	@RequestMapping(value="/index", method=RequestMethod.GET)
+// Alias del requestmapping.
+	
+	@GetMapping({"/index", "/", "/home"})
+	public String index() {
+		
+		return "index";
+	}
+}
